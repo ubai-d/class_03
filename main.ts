1c3b173a74a4ef44e768b9254125680cb670e136
ubai-d <ubaidurrehman14827@gmail.com>
2024-03-04 19:20:44 +0500

    initial commit

---
+++ main.ts
@@ -0,0 +1,132 @@
+// console.log(5 + 3)
+
+// const num1 = 6
+// const num2 = 9
+// console.log(num1 + num2)
+//           6   +   9
+//               15
+
+
+
+
+// const num1 = 10
+// const num2 = 6
+// const sum = num1 + num2
+
+// console.log(sum)
+
+
+
+
+
+
+// const num1 = 10
+// const num2 = 2
+
+// const sub = num1 - num2
+//         //  10       2
+// console.log(sub)
+
+
+
+// const num1 = 10
+// const num2 = 2
+
+// const mul = num1 * num2
+// //           10     2
+// console.log(mul)
+
+
+// const num1 = 10
+// const num2 = 2
+
+// const div = num1 / num2 
+//           10     2
+// console.log(div)
+
+
+
+// const pencils= 25
+// const students = 5
+
+// const mod = pencils % students
+
+// console.log(mod)
+
+
+
+
+// const num1 = 5
+// const num2 = 2
+
+// const power = num1 ** num2
+// //              5       2
+// console.log(power)
+
+
+
+
+
+
+// const firstName = "Ubaid"
+// const lastName = "ur Rehman"
+
+// console.log(firstName + " "+ lastName)
+//            Ubaid   + " " +  ur Rehman
+
+
+
+
+
+// const firstName = "Ubaid"
+// const lastName = "ur Rehman"
+
+// console.log(firstName - lastName)
+
+
+
+// let n = 5
+// n = n + 5
+// n+=5
+// console.log(n)
+
+
+
+
+// console.log(10 == "10");  // ==
+// console.log(10 === "10"); // ===
+// console.log(10 != 5); // !=
+// console.log(5 > 2); // >
+// console.log(5 < 2); // <
+// console.log(5 <= 5); // <
+// console.log(2 <= 5); // <=
+// console.log(2 >= 5); // >=
+
+
+
+
+let a = 10 + 20 // 30
+a = a + 20
+// a = 30 + 20
+// a = 50
+a // 50
+
+
+
+//let   a  =    30
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
